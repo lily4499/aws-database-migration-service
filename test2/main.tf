95c0caccfd585c05573083fc476c891606612e03
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-05-07 12:53:17 -0400

    Update main.tf

--- test2/main.tf
+++ test2/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region = "eu-west-2" # Replace with your desired region
+  region = "us-east-1" # Replace with your desired region
 }
 
 resource "aws_db_instance" "my_database" {
-  identifier           = "myseconddb"
+  identifier           = "target-db"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7" # Replace with your desired MySQL version
   instance_class       = "db.t2.micro" # Replace with your desired instance type
   username             = "admin" # Replace with your desired username
-  password             = "2ndpassword" # Replace with your desired password
+  password             = "abc123" # Replace with your desired password
   parameter_group_name = "default.mysql5.7" # Replace with your desired parameter group
 
   # Replace with your preferred settings for the following parameters if needed

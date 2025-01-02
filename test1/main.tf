f25921cd1d695dcee052fb644513b3a42532bbde
lily4499 <77320224+lily4499@users.noreply.github.com>
2025-01-02 16:39:19 -0500

    Update main.tf

--- test1/main.tf
+++ test1/main.tf
@@ -8,10 +8,9 @@ resource "aws_db_instance" "my_database" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7" # Replace with your desired MySQL version
-  instance_class       = "db.t2.micro" # Replace with your desired instance type
-#  name                 = "my-mysql-db" # Replace with your desired database name
+  instance_class       = "db.t3.micro" # Replace with your desired instance type
   username             = "admin" # Replace with your desired username
-  password             = "abc123" # Replace with your desired password
+  password             = "abc123abc" # Replace with your desired password
   parameter_group_name = "default.mysql5.7" # Replace with your desired parameter group
 
   # Replace with your preferred settings for the following parameters if needed
@@ -19,3 +18,7 @@ resource "aws_db_instance" "my_database" {
   publicly_accessible = true
 }
 
+
+output "rds_endpoint" {
+  value = aws_db_instance.my_database.endpoint
+}
